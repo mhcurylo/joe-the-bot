cf5d9ce85745dbc152607ca79dd13c0a40133c86
mhcurylo <mhcurylo@gmail.com>
2016-09-25 01:48:09 +0100

    Writing pathfinding

--- src/Bot.hs
+++ src/Bot.hs
@@ -6,7 +6,7 @@ module Bot
 import Vindinium
 
 import System.Random (getStdRandom, randomR)
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, isJust, isNothing)
 import Control.Monad (liftM)
 import Control.Monad.IO.Class (liftIO)
 
@@ -15,8 +15,8 @@ bot = myBot
 
 myBot :: Bot
 myBot state = do
-  liftIO $ putStrLn $ show (stateHero state)
-  return Stay
+  liftIO $ putStrLn $ show $ nearestGold state
+  return North
 
 randomBot :: Bot
 randomBot _ = liftM fromJust $ liftIO $ pickRandom [Stay, North, South, East, West]
@@ -39,3 +39,25 @@ pickRandom [] = return Nothing
 pickRandom xs = do
     idx <- getStdRandom (randomR (0, length xs - 1))
     return . Just $ xs !! idx
+
+nearestGold :: State -> [Dir]
+nearestGold state = connections (gameBoard $ stateGame state) (heroPos $ stateHero state)
+
+connections :: Board -> Pos -> [Dir]  
+connections b p = filter (canTraverseTo b p) [North, South, West, East]
+
+canTraverseTo :: Board -> Pos -> Dir -> Bool
+canTraverseTo b p d  
+  | isJust t = not (fromJust t == WoodTile)
+  | isNothing t = False
+  where
+    t = tileAt b $ dirToPosition b p d
+
+dirToPosition :: Board -> Pos -> Dir -> Pos
+dirToPosition  b p@(Pos x y) d 
+  | d == West = Pos (x-1) y
+  | d == East = Pos (x+1) y
+  | d == North = Pos x (y-1)
+  | d == South = Pos x (y+1)
+
+
